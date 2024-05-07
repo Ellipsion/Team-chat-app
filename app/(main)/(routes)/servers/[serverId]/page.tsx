aa1cec5a00f7bb669db524d7e4e56c82e9473245
Ashish Kumawat <ashishskyblue@gmail.com>
2024-05-07 17:52:57 +0530

    add server detail page

---
+++ app/(main)/(routes)/servers/[serverId]/page.tsx
@@ -0,0 +1,9 @@
+import { FC } from "react";
+
+interface ServerDetailPageProps {}
+
+const ServerDetailPage: FC<ServerDetailPageProps> = ({}) => {
+  return <div>ServerDetailPage</div>;
+};
+
+export default ServerDetailPage;
